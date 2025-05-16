e65fe8fc23ff37d312c00f0c4518e877d33f2db6
Thanayut Mungumklang <45162648+thanayut-m@users.noreply.github.com>
2025-05-16 14:46:37 +0700

    update

--- services/LabResult.js
+++ services/LabResult.js
@@ -3,6 +3,7 @@ const TimeHelper = require("../utils/TimeHelper");
 const logger = new Logger();
 const { query_db } = require("./func/ConnectDb");
 const moment = require("moment");
+const fileType = require("file-type");
 
 const labResult = async (jsonData) => {
   try {
@@ -10,7 +11,10 @@ const labResult = async (jsonData) => {
       throw new Error("Missing file data in input JSON");
     }
     let lab_order_id = jsonData.orderNumber.toString().slice(-6);
-    const pdf = Buffer.from(jsonData.labs[0].filebase64[0].filedata, "base64");
+    const buffer = Buffer.from(
+      jsonData.labs[0].filebase64[0].filedata,
+      "base64"
+    );
     const dateTime = moment().format("YYYY-MM-DD");
     const result = await query_db(
       "SELECT MAX(lab_order_image_pdf_id) AS maxId FROM lab_order_image_pdf"
@@ -21,17 +25,40 @@ const labResult = async (jsonData) => {
       [lab_order_id]
     );
     const nextPageNo = pageResult[0]?.maxPage ?? 0;
-    const insertPDF = await query_db(
-      `INSERT INTO lab_order_image_pdf (
+
+    const fileInfo = await fileType.fromBuffer(buffer);
+    const fileExt = fileInfo ? fileInfo.ext : "unknown";
+    console.log(fileExt);
+
+    if (fileExt === "pdf") {
+      console.log("File type PDF.");
+      await query_db(
+        `INSERT INTO lab_order_image_pdf (
         lab_order_image_pdf_id,
         lab_order_image_pdf_file,
         lab_order_id,
         lab_order_image_pdf_date,
         lab_order_image_pdf_page
       ) VALUES (?,?,?,?,?)`,
-      [maxid + 1, pdf, lab_order_id, dateTime, nextPageNo + 1]
-    );
-    console.log("Insert success : ", jsonData);
+        [maxid + 1, buffer, lab_order_id, dateTime, nextPageNo + 1]
+      );
+      console.log("Insert Type PDF success : ", jsonData);
+    } else if (fileExt === "jpg") {
+      console.log("File type JPG.");
+      await query_db(
+        `INSERT INTO lab_order_image(
+          lab_order_image_id,
+          lab_order_image_page_no,
+          lab_order_image_picture,
+          lab_order_id,
+          lab_order_image_name
+      )VALUES (?,?,?,?,?)`,
+        [maxid + 1, nextPageNo + 1, buffer, lab_order_id, dateTime]
+      );
+      console.log("Insert Type JPG success : ", jsonData);
+    } else {
+      console.error("Unsupported file type:", fileExt);
+    }
 
     const newYear = TimeHelper.getYear();
     const newMonth = TimeHelper.getMonth();
