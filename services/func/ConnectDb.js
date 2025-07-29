0f8b42a7b78363a3f6fdb23ba3b9ea5301e37f0e
Thanayut Mungumklang <45162648+thanayut-m@users.noreply.github.com>
2025-07-29 11:41:40 +0700

    Update DB env variable names and add port support

--- services/func/ConnectDb.js
+++ services/func/ConnectDb.js
@@ -2,10 +2,11 @@ const mysql = require("mysql2/promise");
 require("dotenv").config();
 
 const connectDb = mysql.createPool({
-  host: process.env.HOSTNAME,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: parseInt(process.env.DB_PORT) || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
